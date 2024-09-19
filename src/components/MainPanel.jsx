d398d841f879979e7c91b8576cb0510bc72576c7
Henrique Gouveia <henriquesgo42@gmail.com>
2024-09-19 16:05:42 -0300

    Modificacao simples do index.html e utilizacao do MainPanel.jsx para testes de implementacao do NewTransaction.jsx

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -1,6 +1,7 @@
 import Month from "./Month"
 import { useState, useContext } from "react";
 import { EyeContext } from "../Contexts/EyeContext.jsx";
+import NewTransaction from "./NewTransaction.jsx";
 
 const MainPanel = () =>{
     return(
@@ -8,7 +9,8 @@ const MainPanel = () =>{
             <div className="flex flex-col mt-16 ml-36 overflow-auto w-screen h-screen bg-gray-500 p-8">
                 <div className="custom-md:flex-row custom-md:space-x-6 custom-md:space-y-0">
                     <Month />
-                </div>
+                    <NewTransaction type="revenue" value="3000"/>
+                </div> 
             </div>
         </>
     )
