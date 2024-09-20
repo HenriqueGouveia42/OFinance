a7fc41a0d586e9cdfdfbf20c1a32cb33954ae2e8
Henrique Gouveia <henriquesgo42@gmail.com>
2024-09-20 01:53:01 -0300

    Correcao de props

--- src/components/MainPanel.jsx
+++ src/components/MainPanel.jsx
@@ -9,7 +9,7 @@ const MainPanel = () =>{
             <div className="flex flex-col mt-16 ml-36 overflow-auto w-screen h-screen bg-gray-500 p-8">
                 <div className="custom-md:flex-row custom-md:space-x-6 custom-md:space-y-0">
                     <Month />
-                    <NewTransaction type="revenue" value="3000"/>
+                    <NewTransaction type="revenue"/>
                 </div> 
             </div>
         </>
