f441d215204e4ab3fcde6ec7c5400dfe69d2e4b1
Henrique Gouveia <henriquesgo42@gmail.com>
2024-09-20 02:02:57 -0300

    Correcao de prop e ajuste da janela possível de altura de largura

--- src/components/NewTransaction.jsx
+++ src/components/NewTransaction.jsx
@@ -1,23 +1,34 @@
 import { FaArrowCircleLeft } from "react-icons/fa";
 import Numpad from "./Numpad.jsx";
-const NewTransaction = ({type, value}) => {
+import TransactionDetails from "./TransactionDetails.jsx";
+import { useState } from "react";
+
+const NewTransaction = ({type}) => {
+
+    const [visible, setVisible] = useState(false);
+    const handleVisibleClick = () =>{
+        setVisible(prevValue => !prevValue)
+    }
     return(
         <>
-            <div className= "flex flex-col w-[15rem] h-[32rem]">
+            <div className= "flex flex-col min-w-[15rem] max-w-[30rem] min-h-[32rem] max-h-[64rem]">
                 <div className={`flex flex-col w-full h-1/5 ${type === 'revenue' ? 'bg-green-500' : 'bg-red-500'}`}>
                         <div className="flex items-center">
                             <buttom className="p-3 rounded-3xl hover:cursor-pointer"><FaArrowCircleLeft color="white" size="25"/></buttom>
-                            <h1 className="font-medium text-white">Nova receita</h1>
+                            <h1 className="font-medium text-white">Nova Receita</h1>
                         </div>
                     <div className="flex items-center">
                             <div className="flex flex-col">
                                 <h1 className="text-xs text-white">Valor da Receita</h1>
-                                <buttom className="text-lg text-white hover:cursor-pointer hover:bg-green-600 ">{((parseInt(value))/100).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</buttom>
+                                <button className="text-lg text-white hover:cursor-pointer hover:bg-green-600"
+                                onClick={()=>handleVisibleClick()}>
+                                {((parseInt('150'))).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</button>
                             </div>
                         </div>
                 </div>
                 <div className="flex flex-col w-full h-4/5 bg-white">
-                    <Numpad/>  
+                    {visible && <Numpad/> }
+                    {!visible && <TransactionDetails/>}
                 </div>
             </div>
         </>
