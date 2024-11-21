8d6ed210295746207be02fc68f2e87b4a8eaa9ff
Henrique Gouveia <henriquesgo42@gmail.com>
2024-11-20 23:35:35 -0300

    Implementacao do array de meses e das funções necessarias ao uso do DropdownMenu

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
 import MonthContent from "./MonthContent"
 import { IoIosArrowDown } from "react-icons/io";
 
 
 
 const Month = () =>{
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const toggleDropdown = () =>{
+        setIsDropdownOpen((prevState) => !prevState)
+    }
+
+    const [selectedMonth, setSelectedMonth] = useState("Abril");
+    const handleMonthSelection = (month) =>{
+        setSelectedMonth(month);
+        setIsDropdownOpen(false); //Fecha o dropdown apos selecionar um mes
+    }
+
+    const months = [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+    
     return(
-        <div className="flex flex-col justify-start items-center w-auto">
-            <div className="flex items-auto">
-                <h1 className="mb-3  text-white text-xs font-bold">Maio</h1>
-                <h2><IoIosArrowDown size={20} className="ml-4 bg-white rounded-2xl
-                hover:bg-green-600 hover:text-white hover:rounded-xl hover:cursos-pointer"/></h2>
-            </div>
+        <div className="relative flex flex-col justify-start items-center w-auto">
+            <button
+             onClick={toggleDropdown}
+             className="flex items-auto p-2  hover:bg-green-600 hover:rounded-xl items-center" >
+                <h1 className="text-white text-xs font-bold">{selectedMonth}</h1>
+                <h2><IoIosArrowDown size={20} className="ml-4 bg-white rounded-2xl"/></h2>
+            </button>
+            {isDropdownOpen &&
+                <div className="absolute top-12 bg-white rounded-xl shadow-lg p-2 min-w-[10rem]">
+                    {months.map((month) => (
+                        <button
+                            key={month}
+                            className="block w-full text-left p-2 text-sm hover:bg-gray-200 rounded"
+                            onClick={() => handleMonthSelection(month)}
+                        >
+                            {month}
+                        </button>
+                    ))}
+                </div>
+            }
             <div className=" bg-white rounded-3xl p-4 min-w-[10rem]">
                     <MonthContent />
             </div>
