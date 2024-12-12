9082714b32a5a5c2c71a5c64f2812de7367e7fed
Henrique Gouveia <henriquesgo42@gmail.com>
2024-12-12 16:35:09 -0300

    div que virou button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,10 @@ const HeaderIcon = ( { icon } ) => (
 const Header = () =>{
     return(
         <div className=" fixed top-0 left-0 z-10 bg-primary h-16 w-full text-white flex items-center">
-            <div className="ml-7 flex items-center pl-2">
+            <button className="ml-7 flex items-center pl-2">
                 <HeaderIcon icon={<IoMenuOutline size="16"/>}/>
                 <img src={erplogo} className="ml-5"></img>
-            </div>
+            </button>
         </div>
     )
 }
