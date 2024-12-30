5a78a5f9510e0087cc1e76dfac3fcc6ddeff7ddd
Henrique Gouveia <henriquesgo42@gmail.com>
2024-12-30 19:34:56 -0300

    Ajustes na documentacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 
 import { EyeProvider } from "./Contexts/EyeContext.jsx";
 import { TransactionTypeProvider } from "./contexts/TransactionTypeContext.jsx";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import ProtectedRoute from "./routes/ProtectedRoute.jsx";
+import PublicRoute from "./routes/PublicRoute.jsx";
 
-import SideBar from "./components/SideBar.jsx"
-import Header from "./components/Header"
-import MainPanel from "./components/MainPanel";
+import SignUp from './SignUp/SignUp.jsx'
+import Login from './Login/Login.jsx'
+
+import Month from "./components/Month.jsx";
+import NewTransaction from "./components/NewTransaction.jsx";
+import Accounts from "./components/Accounts.jsx"
+import CreditCards from "./components/CreditCards.jsx";
+import NewExpenseOrRevenueType from "./components/NewExpenseOrRevenueType.jsx";
+import NewAccountType from "./components/NewAccountType.jsx";
+
+import LoggedInLayout from "./components/LoggedInLayout.jsx";
 
 function App() {
   return (
-    <TransactionTypeProvider>
-      <div className="flex flex-col">
-      <Header />
-        <div className="grid grid-cols-2">
-          <SideBar />
-          <EyeProvider>
-            <MainPanel/>
-          </EyeProvider>
-        </div>
-      </div>
-    </TransactionTypeProvider>
+    <Router>
+      <AuthProvider>
+        <TransactionTypeProvider>
+        <Routes>
+              {/*Rotas publicas*/}
+              <Route path="/signup" element={<PublicRoute><SignUp/></PublicRoute>}/>
+              <Route path="/login" element={<PublicRoute><Login/></PublicRoute>}/>
+              
+              {/*Rotas protegidas*/}
+          <Route path="/" element={<ProtectedRoute><EyeProvider><LoggedInLayout/></EyeProvider></ProtectedRoute>}> 
+            <Route path="month" element={<Month/>}/>
+            <Route path="newtransaction" element={<NewTransaction/>}/>
+            <Route path="accounts" element={<Accounts/>}/>
+            <Route path="credit-cards" element={<CreditCards/>}/>
+            <Route path="new-expense-or-revenue-type" element={<NewExpenseOrRevenueType/>}/>
+            <Route path="new-account-type" element={<NewAccountType/>}/>
+          </Route>
+        </Routes>
+        </TransactionTypeProvider>
+      </AuthProvider>
+    </Router>
   )
 }
 export default App
