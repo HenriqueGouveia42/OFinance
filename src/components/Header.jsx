4caa1350dc5dda8595b31bafaa105dfe49938edc
Henrique Gouveia <henriquesgo42@gmail.com>
2025-01-17 01:50:15 -0300

    Implementacao do Hook useEffect, que, sempre que a pagina eh recarregada, le o objeto 'data' armazenado no payload do token jwt armazenado no localStorage. Exibicao dinamica do nome de usuario, anexado em data.name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,10 @@ import { IoIosExit } from "react-icons/io";
 import erplogo from "../assets/images/ofinancelogo.png";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+
 
 const HeaderIcon = ( { icon } ) => (
     <button className="header-icon">
@@ -11,6 +15,29 @@ const HeaderIcon = ( { icon } ) => (
 ) 
 
 const Header = () =>{
+    const getPayLoadData = () =>{
+        const token = localStorage.getItem('token');
+        if(!token){
+            console.error('Token nao encontrado no localStorage');
+            return null
+        }
+        try{
+            const decoded = jwtDecode(token);
+            return decoded;
+        }catch(error){
+            console.error('Erro ao decodificar o token JWT: ', error);
+        }
+    }
+    
+    const [username, setUsername] = useState('');
+    
+    
+    useEffect(()=>{
+        const data = getPayLoadData();
+        if(data){
+            setUsername(data.name);
+        }
+    }, []);
     const navigate = useNavigate();
     const {logout} = useAuth()
     return(
@@ -18,9 +45,10 @@ const Header = () =>{
             <button className="ml-7 flex items-center pl-2" onClick={() =>{
                 navigate("/month");
             }}>
-                <HeaderIcon icon={<IoMenuOutline size="16"/>}/>
+                <HeaderIcon icon={<IoMenuOutline size="24"/>}/>
                 <img src={erplogo} className="ml-5" />
             </button>
+            <p>{`Bem vindo, ${username}`}</p>
             <button className="flex items-center hover:bg-slate-400 hover: rounded-3xl mr-10" onClick={()=>logout()}>
                 <IoIosExit size={"50"}/>
                 <p>Sair</p>
