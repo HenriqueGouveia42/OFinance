1271389003b07e2c8218175bec1ee2fa9d0f1e8f
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-04 14:10:56 -0300

    Primeiro prototipo do componente

--- src/components/CreditCards.jsx
+++ src/components/CreditCards.jsx
@@ -1,6 +1,8 @@
 const CreditCards = () =>{
     return(
-        <>Cartoes de credito!</>
+        <div className="bg-maingray w-4/5 h-4/5 rounded-3xl p-3 justify-items-center">
+            <h1>Cartoes de credito</h1>
+        </div>
     )
 }
 export default CreditCards
