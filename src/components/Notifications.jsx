d3b600f950fa7a210fb261dbf60054f63be1df69
Henrique Gouveia <henriquesgo42@gmail.com>
2025-02-04 17:47:12 -0300

    Correção  na importação do contexto e pequena alteração de design

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -6,9 +6,9 @@ import { EyeContext } from "../Contexts/EyeContext";
 
 const NotificationsContent = ({ type, quantity, amount }) => {
     const isIncome = type === 'income';
-    const {isVisible, toggleVisibility} = useContext(EyeContext);
+    const {isVisible} = useContext(EyeContext);
     return (
-        <div className="flex flex-col bg-gray-300 p-2 rounded-2xl mx-3">
+        <div className="flex flex-col bg-gray-300 p-2 rounded-2xl mx-3 hover:bg-slate-400">
             <div className="flex items-center">
                 {isIncome ? <FaArrowCircleUp size={14} /> : <FaArrowCircleDown size={14} /> }
                 <h1 className={`rounded-2xl p-1 ml-16 items-center text-xs ${isIncome ? 'bg-green-400' : 'bg-red-400'}`}>
@@ -16,7 +16,7 @@ const NotificationsContent = ({ type, quantity, amount }) => {
                 </h1>
             </div>
             <div>
-                <div className="text-xs font-bold">
+                <div className="text-xs font-bold ">
                     <h1>{isIncome ? 'Receitas Pendentes' : 'Despesas Pendentes'}</h1>
                     <h1 className={isIncome ? 'text-green-400' : 'text-red-400'}>
                         {isVisible ? amount.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) : ''}
@@ -30,11 +30,11 @@ const NotificationsContent = ({ type, quantity, amount }) => {
 
 const Notifications = () => {
     
-    let qntd_receitas_pendentes = 12;
-    let receitas_pendentes = 1503;
+    let qntd_receitas_pendentes = 11;
+    let receitas_pendentes = 151;
 
-    let qntd_despesas_pendentes = 7;
-    let despesas_pendentes = 3000;
+    let qntd_despesas_pendentes = 8;
+    let despesas_pendentes = 1503;
 
     return (
         <div className="flex flex-col items-center">
@@ -44,12 +44,15 @@ const Notifications = () => {
             </div>
             <div className="mt-5 flex justify-start">
                 <div className="flex justify-center">
-                    {qntd_receitas_pendentes > 0 && (
-                        <NotificationsContent type="income" quantity={qntd_receitas_pendentes} amount={receitas_pendentes} />
-                    )}
-                    {qntd_despesas_pendentes > 0 && (
+                    {
+                        qntd_receitas_pendentes > 0 ? 
+                        (<NotificationsContent type="income" quantity={qntd_receitas_pendentes} amount={receitas_pendentes} />) 
+                        :
+                        <div className="bg-gray-300 p-2 rounded-2xl mx-3 w-36 text-sm">Nenhuma receita pendente</div>
+                    }
+                    {qntd_despesas_pendentes > 0 ? (
                         <NotificationsContent type="expense" quantity={qntd_despesas_pendentes} amount={despesas_pendentes} />
-                    )}
+                    ) : <div className="bg-gray-300 p-2 rounded-2xl mx-3 w-36 text-sm">Nenhuma despesa pendente</div>}
                 </div>
             </div>
         </div>
