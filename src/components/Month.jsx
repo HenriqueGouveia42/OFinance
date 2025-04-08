c0ae529bc1804347e9b240caad65d88f754cf1b9
Henrique Gouveia <henriquesgo42@gmail.com>
2025-04-08 20:49:00 -0300

    Agora o endereço do servidor é lindo de uma variavel de ambiente

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -4,6 +4,8 @@ import { IoIosArrowDown } from "react-icons/io";
 import { SlArrowLeft } from "react-icons/sl";
 import { SlArrowRight } from "react-icons/sl";
 
+import LoadingWrapper from "../wrappers/LoadingWrapper";
+
 const Month = () =>{
 
     const months = [
@@ -28,17 +30,18 @@ const Month = () =>{
                 year: year
             }).toString();
 
-            const response = await fetch(`http://localhost:5000/transaction/readMonthTransaction?${queryParams}`,{
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/transaction/readMonthTransaction?${queryParams}`,{
                 method: 'GET',
                 credentials: 'include', //Permite o envio de cookies Http Only
             });
+
             if(response.ok){
                 const data = await response.json();
                 setMonthYearTransactions(data);
+                
             }else{
                 console.error('Falha ao buscar transacoes')
             }
-            
         }catch(error){
             console.error('Erro ao ler as transacoes do mes: ', error)
         }
@@ -64,38 +67,42 @@ const Month = () =>{
         setYear(prevYear => prevYear - 1 );
     }
     return(
-        <div className="relative flex flex-col justify-start items-center min-w-96 min-h-96">
-            <div className="justify-items-center">
-                <div className="flex space-x-3 items-center justify-center p-1 bg-slate-400 rounded-full">
-                    <button className="p-1 bg-slate-400 rounded-full hover:bg-slate-600" onClick={()=>decrementYear()}><SlArrowLeft /></button>
-                    <h1 className="text-white text-xl font-extrabold">{year}</h1>
-                    <button className="p-1 bg-slate-400 rounded-full hover:bg-slate-600" onClick={()=>incrementYear()}><SlArrowRight /></button>
+        <div className="bg-maingray w-4/5 h-4/5 rounded-3xl p-3 justify-items-center">
+            <div className="relative flex flex-col justify-start items-center min-w-96 min-h-96">
+                <div className="flex justify-items-center">
+                    <div className="flex space-x-3 items-center justify-center p-1 bg-slate-400 rounded-full">
+                        <button className="p-1 bg-slate-400 rounded-full hover:bg-slate-600" onClick={()=>decrementYear()}><SlArrowLeft /></button>
+                        <h1 className="text-white text-xl font-extrabold">{year}</h1>
+                        <button className="p-1 bg-slate-400 rounded-full hover:bg-slate-600" onClick={()=>incrementYear()}><SlArrowRight /></button>
+                    </div>
+                    <button
+                        onClick={toggleMonthDropdown}
+                        className="flex items-auto p-2  hover:bg-green-600 hover:rounded-xl items-center" >
+                        <h1 className="text-white text-xs font-bold">{month}</h1>
+                        <h2><IoIosArrowDown size={20} className="ml-4 bg-white rounded-2xl"/></h2>
+                    </button>
                 </div>
-                <button
-                    onClick={toggleMonthDropdown}
-                    className="flex items-auto p-2  hover:bg-green-600 hover:rounded-xl items-center" >
-                    <h1 className="text-white text-xs font-bold">{month}</h1>
-                    <h2><IoIosArrowDown size={20} className="ml-4 bg-white rounded-2xl"/></h2>
-                </button>
-            </div>
-            {isMonthDropdownOpen &&
-                <div className="absolute top-12 bg-white rounded-xl shadow-lg p-2 min-w-[10rem]">
-                    {months.map((month) => (
-                        <button
-                            key={month}
-                            className="block w-full text-left p-2 text-sm hover:bg-gray-200 rounded"
-                            onClick={() => handleMonthSelection(month)}
-                        >
-                            {month}
-                        </button>
-                    ))}
+                {isMonthDropdownOpen &&
+                    <div className="absolute top-12 bg-white rounded-xl shadow-lg p-2 min-w-[10rem]">
+                        {months.map((month) => (
+                            <button
+                                key={month}
+                                className="block w-full text-left p-2 text-sm hover:bg-gray-200 rounded"
+                                onClick={() => handleMonthSelection(month)}
+                            >
+                                {month}
+                            </button>
+                        ))}
+                    </div>
+                }
+                <div className=" bg-white rounded-3xl p-4 mt-5">
+                        <LoadingWrapper children={
+                            <MonthContent monthYearTransaction={monthYearTransactions} month={month} year={year} />}>
+                        </LoadingWrapper>
+                        
                 </div>
-            }
-            <div className=" bg-white rounded-3xl p-4">
-                    <MonthContent monthYearTransaction={monthYearTransactions} />
             </div>
         </div>
-        
     )
 }
 export default Month
